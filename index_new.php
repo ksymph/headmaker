15488ef238e4cab0661643c4efd79f853d9ad77b
KSymph <flicsmo@vivaldi.net>
2024-07-01 17:23:45 -0400

    add basic style and layout

--- index_new.php
+++ index_new.php
@@ -4,13 +4,48 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>headmaker</title>
-	<link rel="stylesheet" href="./style.css">
-	<link rel="icon" href="./favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="style_new.css">
 </head>
 
 <body>
 	<main>
-		<h1>Hello World</h1>
+		<header>
+			<h1>headmaker</h1>
+			<div id="intro-desc">
+				<p>An easier way to get a<span class="mono">&lt;head></span></p>
+				<p>WIP</p>
+				<p><a href="https://github.com/Flicsmo/headmaker">GitHub</a></p>
+			</div>
+		</header>
+
+		<section id="headmaker-app">
+			<section id="left-pane">
+
+			</section>
+			<section id="right-pane">
+				<div id="output-box">
+					<span class="link" id="copy-code">Copy</span>
+					<div id="output-container">
+						<code id="output-code">
+							This requires JavaScript!
+						</code>
+					</div>
+				</div>
+			</section>
+		</section>
+
+		<footer>
+			<p>Thank you <a href="https://htmlhead.dev">htmlhead.dev</a> for the concise info and descriptions</p>
+			<p>Further reading:</p>
+			<ul>
+				<li><a href="https://html.spec.whatwg.org/multipage/semantics.html#standard-metadata-names">WHATWG standard metadata names</a></li>
+				<li><a href="https://wiki.whatwg.org/wiki/MetaExtensions">WHATWG non-standard &ltmeta> names</a></li>
+			</ul>
+			<a href="https://creativecommons.org/publicdomain/zero/1.0/">
+				<img src="https://i.creativecommons.org/p/zero/1.0/88x31.png" alt="CC0">
+			</a>
+		</footer>
+
 
 	</main>
 </body>
